3ac513cca171eef7576f0a0c1503610236b0eb0f
mati-nc <mcussi64@gmail.com>
2026-07-04 12:10:05 -0300

    Finaliza controladores de endpoints de productos

--- backend/src/controllers/productos.js
+++ backend/src/controllers/productos.js
@@ -1,44 +1,64 @@
+import productosModel from '../models/productos.js'
+
+
 class productosController {
-
+    
     constructor(){
-
     }
 
     //metodos asincrónicos
-
     async create(req,rest){
         try{
-            rest.status(201).json({status: 'create-ok'});
+            const data = await productosModel.create(req.body);
+            rest.status(201).json(data);
         }catch(e){
             rest.status(500).send(e)
         }
+    }
+    
+    async createMany(req,res){
+        try{
+            const data = await productosModel.createMany(req.body);
+            res.status(201).json(data);
+        }catch(e){
+            res.status(500).send(e);
+        }
+    }
 
-    }
-    async update(req,rest){
-         try{
-            rest.status(201).json({status: 'update-ok'});
-        }catch(e){
-            rest.status(500).send(e)
-        }
-
-    }
-    async delete(req,rest){
-         try{
-            rest.status(201).json({status: 'delete-ok'});
-        }catch(e){
-            rest.status(500).send(e)
-        }
-    }
-    async getAll(req,rest){
-         try{
-            rest.status(201).json({status: 'getall-ok'});
-        }catch(e){
-            rest.status(500).send(e)
-        }
-    }
     async getOne(req,rest){
         try{
-            rest.status(201).json({status: 'getone-ok'});
+            const {id} = req.params;
+            const data = await productosModel.getOne(id);
+            rest.status(201).json(data);
+        }catch(e){
+            rest.status(500).send(e)
+        }
+    }
+
+    async getAll(req,rest){
+         try{
+            const data = await productosModel.getAll();
+            rest.status(201).json(data);
+        }catch(e){
+            rest.status(500).send(e)
+        }
+    }
+
+    async update(req,rest){
+         try{
+            const {id} = req.params;
+            const data = await productosModel.update(id, req.body);
+            rest.status(200).json(data);
+        }catch(e){
+            rest.status(500).send(e)
+        }
+    }
+
+    async delete(req,rest){
+         try{
+            const {id} = req.params;
+            const data = await productosModel.delete(id);
+            rest.status(206).json(data);
         }catch(e){
             rest.status(500).send(e)
         }
